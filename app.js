c527361edfba3518617960ed553e99dcc3ae54e6
Jesus Galvis <jgalviscontreras38@gmail.com>
2024-06-13 11:42:30 -0500

    repaso css

---
+++ app.js
@@ -0,0 +1,10 @@
+//objeto padre en el navegador
+let win = window;
+let dom = document;
+let head = document.head;
+let body = dom.body;
+
+console.log(win);
+console.log(dom);
+console.log(head);
+console.log(body);
